ca2f03c67ae41c8f53b8632c140a0faacd336fd2
shq <sunheqi@jwzg.com>
2021-07-08 22:17:47 +0800

    07-08

--- src/lookup.js
+++ src/lookup.js
@@ -10,3 +10,15 @@ export default function lookup(dataObj,keyName) {
     }
     return dataObj[keyName]
 }
+// export default function lookup(dataObj,keyName) {
+//     // .存在但是首项不为.
+//     if(keyName.indexOf('.')!=-1 && keyName!='.'){
+//         var keys=keyName.split('.')
+//         var temp=dataObj;
+//         for(let i=0;i<keys.length;i++){
+//             temp=temp[keys[i]]
+//         }
+//         return temp
+//     }
+//     return dataObj[keyName]
+// }
